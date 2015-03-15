04a585c051e527b90c09d87f5105ad5c8b7d1d65
xumuhua <123138599@qq.com>
2015-03-15 23:57:03 +0800

    create DateAndTimePickerView and set new post new fragment

---
+++ app/src/main/java/com/ivanchou/ucasdemo/ui/adapter/SimpleUserListAdapter.java
@@ -0,0 +1,40 @@
+package com.ivanchou.ucasdemo.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.zip.Inflater;
+
+/**
+ * Created by Origa on 2015/3/15.
+ */
+public class SimpleUserListAdapter extends BaseAdapter {
+
+    LayoutInflater mInflater;
+    public SimpleUserListAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
